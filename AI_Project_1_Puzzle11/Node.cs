c4ca2aee7db1f0ae68999b098003caa46ea8c102
hossein karimy <hosseinkm01@gmail.com>
2023-10-30 00:30:41 +0330

    fix graph search

--- AI_Project_1_Puzzle11/Node.cs
+++ AI_Project_1_Puzzle11/Node.cs
@@ -3,22 +3,25 @@
     public class Node
     {
         public readonly string puzzle;
-        public readonly int SpaceIndex;
+        public readonly int spaceIndex;
+        public readonly Node? parent;
 
-        public Node(string puzzle, int spaceIndex)
+        public Node(string puzzle, int spaceIndex , Node? parent)
         {
+
             this.puzzle = puzzle;
-            SpaceIndex = spaceIndex;
-        }   
+            this.spaceIndex = spaceIndex;
+            this.parent = parent;
+        }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(puzzle, SpaceIndex);
+            return HashCode.Combine(puzzle, spaceIndex);
         }
 
         public static bool operator ==(Node? left, Node? right)
         {
-            if(left is null || right is null)
+            if (left is null || right is null)
                 return false;
             return left.GetHashCode() == right.GetHashCode();
         }
@@ -36,7 +39,7 @@
             {
                 if (other == null)
                     return false;
-                if (SpaceIndex != other.SpaceIndex)
+                if (spaceIndex != other.spaceIndex)
                     return false;
                 for (int i = 0; i < 12; i++)
                 {
@@ -44,8 +47,114 @@
                         return false;
                 }
                 return true;
+            } else return false;
+        }
+
+        public List<Node> GetActions()
+        {
+
+            if (spaceIndex == 0)
+                return new List<Node>() {
+                 new Node(Swap(0,1),1,this),
+                 new Node(Swap(0,4),4, this)
+                };
+
+           else if (spaceIndex == 1)
+                return new List<Node>() {
+                 new Node(Swap(1,0),0, this),
+                 new Node(Swap(1,2),2, this),
+                 new Node(Swap(1,5),5, this),
+                };
+
+            else if(spaceIndex == 2)
+                return new List<Node>() {
+                 new Node(Swap(2,1),1, this),
+                 new Node(Swap(2,3),3, this),
+                 new Node(Swap(2,6),6, this ),
+                };
+
+            else if (spaceIndex == 3)
+                return new List<Node>() {
+                 new Node(Swap(3,2),2, this),
+                 new Node(Swap(3,7),7, this)
+                };
+
+            else if (spaceIndex == 4)
+                return new List<Node>() {
+                 new Node(Swap(4,0),0, this),
+                 new Node(Swap(4,5),5, this),
+                 new Node(Swap(4,8),8, this),
+                };
+
+            else if (spaceIndex == 5)
+                return new List<Node>() {
+                 new Node(Swap(5,1),1, this),
+                 new Node(Swap(5,4),4, this),
+                 new Node(Swap(5,6),6, this),
+                 new Node(Swap(5,9),9, this),
+                };
+
+            else if (spaceIndex == 6)
+                return new List<Node>() {
+                 new Node(Swap(6,2),2, this),
+                 new Node(Swap(6,5),5, this),
+                 new Node(Swap(6,7),7, this),
+                 new Node(Swap(6,10),10, this),
+                };
+
+            else if (spaceIndex == 7)
+                return new List<Node>() {
+                 new Node(Swap(7,3),3, this),
+                 new Node(Swap(7,6),6, this),
+                 new Node(Swap(7,11),11, this)
+                };
+
+            else if (spaceIndex == 8)
+                return new List<Node>() {
+                 new Node(Swap(8,4),4, this),
+                 new Node(Swap(8,9),9, this)
+                };
+
+            else if (spaceIndex == 9)
+                return new List<Node>() {
+                 new Node(Swap(9,5),5, this),
+                 new Node(Swap(9,8),8, this),
+                 new Node(Swap(9,10),10, this),
+                };
+
+            else if (spaceIndex == 10)
+                return new List<Node>() {
+                 new Node(Swap(10,6),6, this),
+                 new Node(Swap(10,9),9, this),
+                 new Node(Swap(10,11),11, this),
+                };
+
+            else if (spaceIndex == 11)
+                return new List<Node>() {
+                 new Node(Swap(11,7),7, this),
+                 new Node(Swap(11,6),6, this)
+                };
+
+            throw new Exception("SpaceIndex out of range");
+        }
+
+        private string Swap(int first, int second)
+        {
+            char[] chars = puzzle.ToCharArray();
+            (chars[second], chars[first]) = (chars[first], chars[second]);
+            return new string(chars);
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    Console.Write($"{puzzle[i*4 + j]} ");
+                }
+                Console.WriteLine();
             }
-            else return false;
         }
     }
 }
--- AI_Project_1_Puzzle11/Program.cs
+++ AI_Project_1_Puzzle11/Program.cs
@@ -1,4 +1,84 @@
 ﻿using AI_Project_1_Puzzle11;
 
-HashSet<Node> frontier = new();
-HashSet<Node> explored = new();
+Queue<Node> frontier = new();
+var goal = new Node("123456789AB ",11, null);
+
+var root = new Node("123456798 AB",9, null);
+frontier.Enqueue(root);
+
+GraphSearch();
+//TreeSearch();
+
+//void TreeSearch()
+//{
+//    while (frontier.Count > 0)
+//    {
+//        var node = frontier.Dequeue();
+//        if (node.Value == goal)
+//        {
+//            Console.WriteLine($"frontier count = {frontier.Count}");
+//            PrintResult(node);
+//            return;
+//        }
+
+//        var parentValue = node.Value;
+//        double newValue;
+
+//        // *5 
+//        newValue = parentValue * 5;
+//        frontier.Enqueue(new Node(newValue, Operator.MultipleOn5, node));
+
+//        // sqrt
+//        newValue = Math.Sqrt(parentValue);
+//        frontier.Enqueue(new Node(newValue, Operator.Sqrt, node));
+
+//        // floor
+//        newValue = Math.Floor(parentValue);
+//        if (newValue != parentValue)
+//            frontier.Enqueue(new Node(newValue, Operator.Floor, node));
+//    }
+//}
+
+void GraphSearch()
+{
+    HashSet<Node> frontierAndExplored = new();
+
+    while (frontier.Count > 0)
+    {
+        var node = frontier.Dequeue();
+        if (node == goal)
+        {
+            Console.WriteLine($"frontier count = {frontier.Count}");
+            Console.WriteLine($"explored count = {frontierAndExplored.Count}");
+            int level = 0;
+            PrintResult(node, ref level);
+            return;
+        }
+        frontierAndExplored.Add(node);
+
+        foreach (Node n in node.GetActions())
+        {
+            if (frontierAndExplored.Add(n))
+            {
+                frontier.Enqueue(n);
+            }
+        }
+
+    }
+}
+
+return 1;
+
+
+void PrintResult(Node node,ref int level)
+{
+    if (node.parent is null)
+    {
+        Console.WriteLine("start = " + node.puzzle);
+        return;
+    }
+    PrintResult(node.parent, ref level);
+    Console.WriteLine("level: " + ++level);
+    node.Print();
+    Console.WriteLine("____________________________________");
+}
