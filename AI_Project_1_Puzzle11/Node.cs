5e9284f5405cae7c4a710288a5b1099a851976c6
hossein karimy <hosseinkm01@gmail.com>
2023-10-29 16:35:58 +0330

    Add project files.

---
+++ AI_Project_1_Puzzle11/Node.cs
@@ -0,0 +1,47 @@
+﻿namespace AI_Project_1_Puzzle11
+{
+    public class Node
+    {
+        public readonly int?[] puzzle = new int?[12];
+        public readonly int SpaceIndex;
+
+        public Node(int?[] puzzle, int spaceIndex)
+        {
+            this.puzzle = puzzle;
+            SpaceIndex = spaceIndex;
+        }   
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(puzzle, SpaceIndex);
+        }
+
+        public static bool operator ==(Node? left, Node? right)
+        {
+            return left!.GetHashCode == right!.GetHashCode;
+        }
+
+        public static bool operator !=(Node? left, Node? right)
+        {
+            return left!.GetHashCode != right!.GetHashCode;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Node other)
+            {
+                if (other == null)
+                    return false;
+                if (SpaceIndex != other.SpaceIndex)
+                    return false;
+                for (int i = 0; i < 12; i++)
+                {
+                    if (puzzle[i] != other.puzzle[i])
+                        return false;
+                }
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ AI_Project_1_Puzzle11/Program.cs
@@ -0,0 +1,12 @@
+﻿using AI_Project_1_Puzzle11;
+
+var node1 = new Node(new int?[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, null },11);
+var node2 = new Node(new int?[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 , null }, 11);
+
+Console.WriteLine(node1.GetHashCode());
+Console.WriteLine(node2.GetHashCode());
+
+Console.WriteLine(node1 == node2);
+
+HashSet<Node> explored = new();
+
