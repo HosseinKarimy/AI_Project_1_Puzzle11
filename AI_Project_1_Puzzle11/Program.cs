c08a780ac28b0e538c49a71f5471f8a3e194898b
hossein karimy <hosseinkm01@gmail.com>
2023-10-30 01:02:48 +0330

    calculate goad based on root

--- AI_Project_1_Puzzle11/Program.cs
+++ AI_Project_1_Puzzle11/Program.cs
@@ -1,43 +1,34 @@
 ﻿using AI_Project_1_Puzzle11;
 
 Queue<Node> frontier = new();
-var goal = new Node("123456789AB ",11, null);
 
-var root = new Node("123456798 AB",9, null);
+var rootPuzzle = "566274131 AB";
+var goal = new Node(CalculateGoal(rootPuzzle), 11, null);
+var root = new Node(rootPuzzle, 9, null);
 frontier.Enqueue(root);
 
 GraphSearch();
 //TreeSearch();
 
-//void TreeSearch()
-//{
-//    while (frontier.Count > 0)
-//    {
-//        var node = frontier.Dequeue();
-//        if (node.Value == goal)
-//        {
-//            Console.WriteLine($"frontier count = {frontier.Count}");
-//            PrintResult(node);
-//            return;
-//        }
+void TreeSearch()
+{
+    while (frontier.Count > 0)
+    {
+        var node = frontier.Dequeue();
+        if (node == goal)
+        {
+            Console.WriteLine($"frontier count = {frontier.Count}");
+            int level = 0;
+            PrintResult(node, ref level);
+            return;
+        }
 
-//        var parentValue = node.Value;
-//        double newValue;
-
-//        // *5 
-//        newValue = parentValue * 5;
-//        frontier.Enqueue(new Node(newValue, Operator.MultipleOn5, node));
-
-//        // sqrt
-//        newValue = Math.Sqrt(parentValue);
-//        frontier.Enqueue(new Node(newValue, Operator.Sqrt, node));
-
-//        // floor
-//        newValue = Math.Floor(parentValue);
-//        if (newValue != parentValue)
-//            frontier.Enqueue(new Node(newValue, Operator.Floor, node));
-//    }
-//}
+        foreach (Node n in node.GetActions())
+        {
+            frontier.Enqueue(n);
+        }
+    }
+}
 
 void GraphSearch()
 {
@@ -70,7 +61,7 @@ void GraphSearch()
 return 1;
 
 
-void PrintResult(Node node,ref int level)
+void PrintResult(Node node, ref int level)
 {
     if (node.parent is null)
     {
@@ -81,4 +72,13 @@ void PrintResult(Node node,ref int level)
     Console.WriteLine("level: " + ++level);
     node.Print();
     Console.WriteLine("____________________________________");
+}
+
+static string CalculateGoal(string root)
+{
+    char[] chars = root.ToCharArray();
+    Array.Sort(chars);
+    chars = chars.Where((c, i) => i != 0).ToArray();
+    chars = chars.Append(' ').ToArray();
+    return new string(chars);
 }
